43a847bbabf54b912812c05d207e3b404f17a51d
Hasan Chowdhury <shemonc@gmail.com>
2018-05-06 10:46:22 -0400

    lower the max throttle based on rosbag comment

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,7 +20,7 @@ class Controller(object):
         ki = 0.1
         kd = 0.
         mn = 0.  # Minimum throttle value
-        mx = 0.3 # Maximum throttle value
+        mx = 0.2 # Maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
         tau = 0.5 # 1/(2pi*tau) = cutoff frequency
